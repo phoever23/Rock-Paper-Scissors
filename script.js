644adc296c4a0af06360b70e31478ec25445eb6a
TAKA <gaoming93@foxmail.com>
2023-09-26 14:41:18 +0800

    add simple ui to rps game

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-const choices = ["Rock", "Paper", "Scissors"]
-let playerScore = 0
-let computerScore = 0
+const CHOICES = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll("button");
 
 function getComputerChoice() {
-    return choices[Math.floor(Math.random() * 3)]
-}
-
-function getPlayerChoice() {
-    const choice = prompt("Choose one from Rock, Paper and Scissors:")
-    return choice.charAt(0).toUpperCase() + choice.substring(1).toLowerCase()
+    return CHOICES[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection) {
-
     if (playerSelection === "Rock" && computerSelection === "Scissors" || 
         playerSelection === "Paper" && computerSelection === "Rock" ||
         playerSelection === "Scissors" && computerSelection === "Paper") {
@@ -26,22 +21,39 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    const result = playRound(playerSelection,computerSelection)
-    console.log(result)
+const div = document.querySelector(".result");
+const playRoundResult = document.createElement("h4");
+const currentScore = document.createElement("h4");
+const finalResult = document.createElement("h2");
 
+function game() {
+    // play one round and show this round's result
+    const playerSelection = this.innerText;
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection,computerSelection);
+    playRoundResult.innerText = result;
+    div.appendChild(playRoundResult);
+
+    // record scores and show them
     if(result.includes("Win")) {
         playerScore += 1
     }else if(result.includes("Lose")) {
         computerScore += 1
     }
+    currentScore.innerText = `You ${playerScore} : Computer ${computerScore}`;
+    div.appendChild(currentScore);
 
-    console.log(`You ${playerScore} : Computer ${computerScore}`)
+    // game ends if one player reaches 5 points
+    if (playerScore === 5) {
+        finalResult.innerText = "GAME OVER! YOU WIN!";
+        div.appendChild(finalResult);
+        buttons.forEach(button=>button.removeEventListener("click",game));        
+    } else if (computerScore === 5) {
+        finalResult.innerText = "GAME OVER! COMPUTER WINS!";
+        div.appendChild(finalResult);
+        buttons.forEach(button=>button.removeEventListener("click",game));
+    }
 }
 
-for (let i = 0; i < 5; i++) {
-    game();    
-}
-  
+buttons.forEach(button => {button.addEventListener("click",game)});
+
